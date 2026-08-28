6bb7c1bd54eb6b668da286ce0ec24647a4dd8d31
Bilolbek <mamadrayimovbilolbek@gmail.com>
2026-08-28 14:29:07 +0500

    new file added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 console.log("Web serverni boshlash !");
+const { clear } = require("console");
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -16,13 +17,23 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4. Routing code
-app.get("/hello", function(req, res) {
-    res.end(`<h1 style="background: green">Hello world by Mike</h1>`);
-}) ;
+// app.get("/hello", function(req, res) {
+//     res.end(`<h1 style="background: green">Hello world by Mike</h1>`);
+// }) ;
 
-app.get("/gift", function(req, res) {
-    res.end(`<h1 style="background: grey">You are at gifts page !</h1>`);
-}) ;
+// app.get("/gift", function(req, res) {
+//     res.end(`<h1 style="background: grey">You are at gifts page !</h1>`);
+// }) ;
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
+});
+app.get(`/author`, (req, res) => {
+  res.render("author", { user: user });
+});
+app.get(`/`, function (req, res) {
+  res.render("harid");
+});
 
 const server = http.createServer(app);
 const PORT = 3000;
