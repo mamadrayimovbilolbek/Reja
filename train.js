f2ba047aeb98e82020f9de1379fdf4c9b39b9dc3
Bilolbek <mamadrayimovbilolbek@gmail.com>
2026-09-11 14:58:28 +0500

    feat: new task

--- train.js
+++ train.js
@@ -1,19 +1,40 @@
+function findDoublers(str) {
+  const seen = new Set();
+  for (let char of str) {
+    if (seen.has(char)) {
+      return true;
+    }
+    seen.add(char);
+  }
+  return false;
+}
+
+console.log(findDoublers("wednesday")); // true ("d" ikki marta takrorlangan)
+console.log(findDoublers("monday")); // false (barcha harflar noyob)
+
+
+
+
+
+
+
+
 // B- task
-// A- TASK
-console.log(`train task ishga tushdi`);
-console.log("Jack Ma maslahatlari");
+// // A- TASK
+// console.log(`train task ishga tushdi`);
+// console.log("Jack Ma maslahatlari");
 
-const list = [
-  "yaxshi talaba bo'ling", // 0-20
-  "to'g'ri boshliq tanlang va ko'proq o'rganing", // 20-30
-  "mustaqil ishlarshni boshlang", // 30 - 40
-  "siz kuchli bo'lgan narsalarni qiling", // 40 - 50
-  "yoshlarga investitsiya qilish", // 50 -60
-  "dam oling, endi foydasi yo'q", // 60
-];
+// const list = [
+//   "yaxshi talaba bo'ling", // 0-20
+//   "to'g'ri boshliq tanlang va ko'proq o'rganing", // 20-30
+//   "mustaqil ishlarshni boshlang", // 30 - 40
+//   "siz kuchli bo'lgan narsalarni qiling", // 40 - 50
+//   "yoshlarga investitsiya qilish", // 50 -60
+//   "dam oling, endi foydasi yo'q", // 60
+// ];
 
 
-// callback function example
+// // callback function example
 
 /* 
 function maslahatBering(a, callBack) {
@@ -38,22 +59,22 @@ maslahatBering(65, (err, data) => {
 console.log("passed here: 1");
 */
 
-// asynchronous function
-async function maslahatBering(a, callBack) {
-  if (typeof a !== "number") callBack("insert a number", null);
-  else if (a <= 20) return list[0];
-  else if (a > 20 && a <= 30) return list[1];
-  else if (a > 30 && a <= 40) return list[2];
-  else if (a > 40 && a <= 50) return list[3];
-  else if (a > 50 && a <= 60) return list[4];
-  else {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(list[5]);
-      }, 5000);
-    });
-  }
-}
+// // asynchronous function
+// async function maslahatBering(a, callBack) {
+//   if (typeof a !== "number") callBack("insert a number", null);
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 50) return list[3];
+//   else if (a > 50 && a <= 60) return list[4];
+//   else {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(list[5]);
+//       }, 5000);
+//     });
+//   }
+// }
 
 
 // then/catch
@@ -69,48 +90,48 @@ maslahatBering(20)
 console.log("passed here: 1");
 */
 
-// asynchronous/await
-async function run() {
-  let javob = await maslahatBering(25);
-  console.log(javob);
-  javob = await maslahatBering(75);
-  console.log(javob);
-  javob = await maslahatBering(41);
-  console.log(javob);
-}
-run();
+// // asynchronous/await
+// async function run() {
+//   let javob = await maslahatBering(25);
+//   console.log(javob);
+//   javob = await maslahatBering(75);
+//   console.log(javob);
+//   javob = await maslahatBering(41);
+//   console.log(javob);
+// }
+// run();
 
 // Extra tasks
-// 1. Function Declaration
-function countLetters(str, char) {
-    let count = 0;
-    for (const letter of str) {
-      if (letter === char) {
-        count = count + 1;
-      }
-    }
-    return count;
-  }
+// // 1. Function Declaration
+// function countLetters(str, char) {
+//     let count = 0;
+//     for (const letter of str) {
+//       if (letter === char) {
+//         count = count + 1;
+//       }
+//     }
+//     return count;
+//   }
   
-  // 2. Function Expression
-  const countLetters2 = function(str, char) {
-    let count = 0;
-    for (const letter of str) {
-      if (letter === char) {
-        count += 1;
-      }
-    }
-    return count;
-  }
+//   // 2. Function Expression
+//   const countLetters2 = function(str, char) {
+//     let count = 0;
+//     for (const letter of str) {
+//       if (letter === char) {
+//         count += 1;
+//       }
+//     }
+//     return count;
+//   }
   
-  // 3. Arrow Function
-  const countLetters3 = (str, char) => {
-    let count = 0;
-    for (const letter of str) {
-      if (letter === char) count += 1;
-    }
-    return count;
-  }
+//   // 3. Arrow Function
+//   const countLetters3 = (str, char) => {
+//     let count = 0;
+//     for (const letter of str) {
+//       if (letter === char) count += 1;
+//     }
+//     return count;
+//   }
   
   //console.log(countLetters("i'm_mike", "m"));
   //console.log(countLetters2("bilolbek", "b"));
