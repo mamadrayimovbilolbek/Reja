2c40071864ad221388dba1fc34a51c6fb43c814c
Bilolbek <mamadrayimovbilolbek@gmail.com>
2026-09-20 03:13:14 +0900

    fix: deploy proccess

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-// const http = require("http");
-// const app = require("./app");
-
-// const server = http.createServer(app);
-// let PORT = 3000;
-// server.listen(PORT, function () {
-//   console.log(
-//     `The Server is running successfully on port: ${PORT}, http://localhost:${PORT}`,
-//   );
-// });
 
 
 
