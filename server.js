f3b4cb0ea3260800673c47fe6860aa50890d74cc
Bilolbek <mamadrayimovbilolbek@gmail.com>
2026-09-20 01:45:28 +0900

    fix: deploying

--- server.js
+++ server.js
@@ -18,21 +18,21 @@ const mongodb = require("mongodb");
 let db;
 const connectionString = "mongodb+srv://mamadrayimovbilolbek001_db_user:ol85yRXRtVqHvURV@cluster0.4ay2yly.mongodb.net/Reja?=cluster0";
 
-mongodb.connect(connectionString, 
+mongodb.connect(connectionString,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     },
     (err, client) => {
         if (err) console.log("ERROR on connection MongoDB");
-        else{
+        else {
             console.log("MongoDB connection succeed");
             db = client.db();
             // console.log(client);
             module.exports = client;
             const app = require("./app");
             const server = http.createServer(app);
-            const PORT = 3000;
+            const PORT = 6011;
             server.listen(PORT, function () {
                 console.log(`The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`);
             });
